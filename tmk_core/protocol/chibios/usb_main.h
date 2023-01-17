cdcea10c115168ed10d37615ffbf369665fc0e86
mattmasc <matteo.mascione@gmail.com>
2023-01-17 19:10:18 +0100

    Cherry-Pick Keychron's bluetooth_playground changes

--- tmk_core/protocol/chibios/usb_main.h
+++ tmk_core/protocol/chibios/usb_main.h
@@ -36,6 +36,12 @@ void init_usb_driver(USBDriver *usbp);
 /* Restart the USB driver and bus */
 void restart_usb_driver(USBDriver *usbp);
 
+/* Wakeup the USB driver */
+void usb_wakeup(USBDriver *usbp);
+
+/* Start the USB driver */
+void usb_start(USBDriver *usbp);
+
 /* ---------------
  * USB Event queue
  * ---------------
